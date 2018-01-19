2964de20aa68f43cbb82297e0f4465fab39340af
dong <609042238@qq.com>
2018-01-19 16:02:18 +0800

    初始化

---
+++ FakeGPS/FakeGPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FakeGPS
+//
+//  Created by dong on 2018/1/19.
+//  Copyright © 2018年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
